0d14fe1d5543a8f75138ddc5e8c3ec07098192f3
Brian <brolsen2232@gmail.com>
2020-07-30 17:18:52 -0400

    Added basic segmentation

--- main.cpp
+++ main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <librealsense2/rs.hpp>     
 #include <opencv2/opencv.hpp>
+#include "segmentation.cpp"
 using namespace cv;
 int main(int argc, char * argv[]) try
 {
     rs2::colorizer color_map;
     rs2::pipeline p;
-    p.start();
+    rs2::config cfg;
+
+    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16);
+    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8);
+
+    rs2::pipeline_profile profile = p.start(cfg);
     namedWindow("RGB", WINDOW_AUTOSIZE);
     namedWindow("Depth", WINDOW_AUTOSIZE);
     while (waitKey(1) < 0 && getWindowProperty("RGB", WND_PROP_AUTOSIZE) >= 0 &&  getWindowProperty("Depth", WND_PROP_AUTOSIZE) >= 0)
     {
         rs2::frameset frames = p.wait_for_frames();
+       
         rs2::frame depth_Frame = frames.get_depth_frame().apply_filter(color_map);
-        rs2::frame rgb_Frame = frames.get_color_frame();
-        Mat rgbFrame(Size(640, 480), CV_8UC3, (void*)rgb_Frame.get_data(), Mat::AUTO_STEP);
+        rs2::frame color_Frame = frames.get_color_frame();
+        Mat colorFrame(Size(640, 480), CV_8UC3, (void*)color_Frame.get_data(), Mat::AUTO_STEP);
         Mat depthFrame(Size(640, 480), CV_8UC3, (void*)depth_Frame.get_data(), Mat::AUTO_STEP);
-        imshow("RGB",rgbFrame);
+        
+        imshow("RGB",colorFrame);
         imshow("Depth",depthFrame);
+        imshow("Segmented",segmentation(colorFrame, 16));
 
     }
 
---
+++ segmentation.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+Mat segmentation(Mat frame, int clusterCount){
+    Mat _frame(frame.size(), frame.type());
+    Mat points;
+    blur(frame, frame, Size(30, 30));
+    frame.convertTo(points, CV_32FC3);
+    points = points.reshape(3, frame.rows*frame.cols);
+    
+    Mat_<int> clusters(points.size(), CV_32SC1);
+    Mat centers;
+
+    kmeans(points, clusterCount, clusters, TermCriteria(1, 10, 1.0), 1, KMEANS_PP_CENTERS, centers);
+    MatIterator_<Vec3b> it = _frame.begin<Vec3b>(), it_end = _frame.end<Vec3b>();
+    for(int i = 0; it != it_end; it++, i++){
+        Vec3f &rgb = centers.at<Vec3f>(clusters(i),0);
+        (*it)[0] = saturate_cast<uchar>(rgb[0]);
+        (*it)[1] = saturate_cast<uchar>(rgb[1]);
+        (*it)[2] = saturate_cast<uchar>(rgb[2]);
+
+    }  
+
+    return _frame;
+}
